1171bdb7cb090c2163ff6932b05d3b4abf5f9c97
Johann <johann@objekt.stream>
2020-11-29 12:24:59 +0100

    + brackets

--- src/components/ClassComponent.tsx
+++ src/components/ClassComponent.tsx
@@ -13,8 +13,10 @@ export class ClassComponent extends Vue.with(Props) {
   }
 
   render () {
-    return <div onClick={this.inc}>
-      {this.test} {this.count}
-    </div>
+    return (
+      <div onClick={this.inc}>
+        {this.test} {this.count}
+      </div>
+    )
   }
 }
